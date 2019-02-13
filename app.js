314d83da20f8966d19f9cc07c0f8a00484bf957d
Abdul Rehman <abdul-rehman@myseneca.ca>
2019-02-12 19:48:32 -0600

    added abilitiy to reply to threads with rules

--- app.js
+++ app.js
@@ -8,49 +8,47 @@ snooper = new Snooper(
         app_id: process.env.CLIENT_ID,
         api_secret: process.env.CLIENT_SECRET,
         user_agent: 'For r/Seneca moderation',
-
         automatic_retries: true, // automatically handles condition when reddit says 'you are doing this too much'
-        api_requests_per_minute: 60 // api requests will be spread out in order to play nicely with Reddit
+        api_requests_per_minute: 60 // api requests will be spread out in order to play nicely with Reddit rules
     })
 
-/*
-        1- Watch for new comments
-        2- see if user is one of the mods
-        3- see if comment is about rules
-        4- find comment or thread ID
-        5- reply
-*/
-console.log(process.env.REDDIT_USER, process.env.REDDIT_PASS, process.env.CLIENT_ID, process.env.CLIENT_SECRET);
-
-snooper.watcher.getCommentWatcher('seneca') // blank argument or 'all' looks at the entire website
+snooper.watcher.getCommentWatcher('seneca') //watching r/Seneca
     .on('comment', function (comment) {
-        if (comment.data.author == process.env.MOD1) {
-            //check if comment starts with '!'
-            console.log(`working...`)
-            if ((comment.data.body.toUpperCase()).includes('!RULE')) {
-                console.log(`this is about rules!!!!` + comment.data.body)
-                
+        if (comment.data.author == process.env.MOD1 || comment.data.author == process.env.MOD2) { //looking out for mod comments
+            if ((comment.data.body.toUpperCase()).includes('!RULE')) { //seeing if it contains this key
+                console.log(`comment body: `, comment.data.body)
+
+                switch((comment.data.body).toUpperCase()) { //display rules
+                    case "!RULE1":
+                            reply(comment.data.link_id,`Hi ${comment.data.link_author},This page is not officially 
+                            maintained by Seneca staff, therefore any questions regarding costs,
+                            registration, courses... etc are better off answered by contacting admissions.
+                            416.491.5050 ext. 22840`);
+                      break;
+                    case "!RULE2":
+                            reply(comment.data.link_id,"This post was removed for asking questions that should be asked to Seneca Staff");
+                      break;
+                    case "!RULE3":
+                    reply(comment.data.link_id,`Removed for breaking rule 3: No questions about programs and courses`);
+                    break;
+                    case "!RULE4":
+                    reply(comment.data.link_id,`Hi ${comment.data.link_author}, your post was removed for breaking rule 4: No ads for services allowed.\n\nBleep Boop. I am a bot, please don't downvote me :c`);
+                    break;
+                    default:
+                      // code block
+                  }
             }
         }
-        // comment is a object containing all comment data
-        console.log('comment was posted by: ' + comment.data.author)
-        console.log('contents ' + comment.data.body)
-        // or
-        console.log(comment)
     })
     .on('error', console.error);
 
-function replyThread(url,message)
-{
-    snooper.api.post(url,)
+
+function reply(uniqueID,content) { //reply to id provided
+    snooper.api.post("/api/comment", {
+        api_type: "json",
+        text:     content,
+        thing_id: uniqueID
+    }, function (err, statusCode, data) {
+        if (!err) console.log(`just replied to thread`)
+    });
 }
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-    //     snooper.watcher.getPostWatcher('seneca') // blank argument or 'all' looks at the entire website
-    // .on('post', function(post) {
-    //     // comment is a object containing all comment data
-    //     console.log('post was posted by: ' + post.data.author)
-    //     console.log(post)
-    // })
-    // .on('error', console.error)
-    //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-    //watch for comments
